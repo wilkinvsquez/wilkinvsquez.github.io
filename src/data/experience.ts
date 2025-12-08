b7a4ac7601caa9c0861bb57ab7d280c4ca7de13d
wilkinvsquez <wilkinvsquez@gmail.com>
2025-12-08 13:11:42 -0600

    feat(data): created filesto handle all the content and information shown on the portafolio

---
+++ src/data/experience.ts
@@ -0,0 +1,79 @@
+export const experience = {
+    en: [
+        {
+            id: 1,
+            role: "Software Developer",
+            company: "CENTAURO SOLUTIONS",
+            period: "Feb 2023 - Present",
+            description: [
+                "Collaborated in defining architecture, state management strategies, and component-driven design patterns to improve code reusability and scalability.",
+                "Participated in the development of a custom interpreter that allows adding new modules through JSON-based configurations, reducing development time and making the system highly extensible.",
+                "Built a centralized Feature Manager and internal configuration platform using Vue.js, enabling dynamic activation/deactivation of features and optimizing configuration management across multiple internal projects.",
+                "Migrated Pricesmart Application from Ionic/Angular to Ionic/Vue.js, improving scalability.",
+                "Created internal applications using Outsystems to improve processing efficiency.",
+                "Implemented Google Analytics and One Signal to enhance analytics and notification systems.",
+                "Migrated a billing system from React.js to Outsystems, improving processing efficiency."
+            ]
+        },
+        {
+            id: 2,
+            role: "Frontend Web Developer",
+            company: "CLICKLEASE LIMITADA",
+            period: "Jul 2021 – Jan 2023",
+            description: [
+                "Developed e-commerce applications using PHP, JavaScript, and HTML/CSS.",
+                "Led the development of a Shopify application to process credit applications across multiple marketplaces.",
+                "Designed and developed responsive interfaces with Angular 13+, improving the internal application process.",
+                "Developed an SDK in Node.js to integrate leasing buttons into third-party partner sites."
+            ]
+        },
+        {
+            id: 3,
+            role: "Data Entry",
+            company: "AVANTICA TECHNOLOGIES",
+            period: "Oct 2019 – Jan 2021",
+            description: [
+                "Extracted and managed data from HTML pages using CSS selectors in JIRA and proprietary platforms."
+            ]
+        }
+    ],
+    es: [
+        {
+            id: 1,
+            role: "Desarrollador de Software",
+            company: "CENTAURO SOLUTIONS",
+            period: "Feb 2023 - Presente",
+            "description": [
+                "Colaboré en la definición de la arquitectura, estrategias de administración de estado y patrones de diseño basados en componentes para mejorar la reutilización de código y la escalabilidad.",
+                "Participé en el desarrollo de un intérprete personalizado que permite agregar nuevos módulos mediante configuraciones basadas en JSON, reduciendo el tiempo de desarrollo y aumentando la extensibilidad del sistema.",
+                "Desarrollé un Feature Manager centralizado y una plataforma interna de configuración utilizando Vue.js, permitiendo la activación/desactivación dinámica de funcionalidades y optimizando la gestión de configuraciones en múltiples proyectos internos.",
+                "Migré la aplicación de Pricesmart de Ionic/Angular a Ionic/Vue.js, mejorando la escalabilidad.",
+                "Creé aplicaciones internas utilizando OutSystems para mejorar la eficiencia de los procesos.",
+                "Implementé Google Analytics y OneSignal para fortalecer los sistemas de analítica y notificaciones.",
+                "Migré un sistema de facturación de React.js a OutSystems, mejorando la eficiencia en el procesamiento."
+            ]
+
+        },
+        {
+            id: 2,
+            role: "Desarrollador Web Frontend",
+            company: "CLICKLEASE LIMITADA",
+            period: "Jul 2021 – Ene 2023",
+            description: [
+                "Desarrollé aplicaciones de e-commerce utilizando PHP, JavaScript y HTML/CSS.",
+                "Lideré el desarrollo de una aplicación de Shopify para procesar solicitudes de crédito en múltiples marketplaces.",
+                "Diseñé y desarrollé interfaces responsivas con Angular 13+, mejorando el proceso de aplicaciones internas de la empresa.",
+                "Desarrollé un SDK en Node.js para integrar botones para solicitudes de crédito en sitios de socios externos."
+            ]
+        },
+        {
+            id: 3,
+            role: "Data Entry",
+            company: "AVANTICA TECHNOLOGIES",
+            period: "Oct 2019 – Ene 2021",
+            description: [
+                "Extracción y gestión de datos de páginas HTML utilizando selectores CSS en JIRA y plataformas propietarias."
+            ]
+        }
+    ]
+};
